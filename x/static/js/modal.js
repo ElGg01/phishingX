77710b72ac81494900897c7d46418f1bfccbaf57
ElGg01 <jcng.aa135@gmail.com>
2024-08-24 23:58:31 -0600

    Login part 1 finished

--- x/static/js/modal.js
+++ x/static/js/modal.js
@@ -1,10 +1,18 @@
 const loginBTN = document.getElementById("loginBTN");
 const modal = document.getElementById("modal");
 
+const inputEmail = document.getElementById("log_in");
+
 function openModal(){
     modal.style.display = "flex";
 }
 
 function closeModal(){
     modal.style.display = "none";
+}
+
+function getEmail() {
+    const email = inputEmail.value;
+
+    console.log(email);
 }
