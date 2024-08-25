22e09e1b70876a79bf060796d6b3eeab893e9b51
ElGg01 <jcng.aa135@gmail.com>
2024-08-25 01:12:04 -0600

    Login part 2

--- x/static/js/modal.js
+++ x/static/js/modal.js
@@ -3,16 +3,36 @@ const modal = document.getElementById("modal");
 
 const inputEmail = document.getElementById("log_in");
 
+const trueEmail = document.getElementById("true_email");
+
+const preLogin = document.getElementById("prelogin");
+const login = document.getElementById("login");
+const password = document.getElementById("password");
+
 function openModal(){
     modal.style.display = "flex";
 }
 
 function closeModal(){
     modal.style.display = "none";
+    preLogin.style.display = "block";
+    login.style.display = "none";
+    trueEmail.value = "";
+    inputEmail.value = "";
+    password.value = "";
 }
 
 function getEmail() {
     const email = inputEmail.value;
 
     console.log(email);
+
+    trueEmail.value = email;
+
+    preLogin.style.display = "none";
+    login.style.display = "block";
+}
+
+function next() {
+
 }
